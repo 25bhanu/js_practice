fafb850dd89cf78072503be1792c5275168b964c
25bhanu <bhanupratap252002@gmail.com>
2026-01-28 18:30:31 +0530

    Add JS practice projects

---
+++ BMI_genrator/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        alert(`Please provide a valid height ${height}`);
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        alert(`Please Provide a valid weight ${weight}`);
+    }
+    else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        // result.innerHTML = `<span>Your BMI - ${bmi}</span>`;
+        result.innerHTML = `<span>Your BMI is ${bmi}</span><p>${bmi < 18.6 ? 'You are Under Weight' :
+                bmi >= 18.6 && bmi < 24.9 ? 'You are in Normal Range' :
+                    'You are Over Weight'
+            }</p>`;
+    }
+
+})
